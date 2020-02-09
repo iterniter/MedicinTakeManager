bab727bc3733a62d2a86e34cc4badfa0776b72f7
Ben <kbennani@rheinmetall.ca>
2020-02-08 23:46:00 -0500

    create action on buttons

--- MedecinTakeManager/app/src/main/java/com/example/medecintakemanager/MainActivity.java
+++ MedecinTakeManager/app/src/main/java/com/example/medecintakemanager/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.medecintakemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addButton;
+    Button historyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Buttons
+        addButton = findViewById(R.id.ButtonAddData);
+        historyButton = findViewById(R.id.ButtonHistory);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddDataActivity.class));
+            }
+        });
+
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,HistoryActivity.class));
+            }
+        });
     }
 }
