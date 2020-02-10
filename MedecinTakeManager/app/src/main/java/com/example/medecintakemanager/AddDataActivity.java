be991dfe5bfcade63839b2a4b1b7cf6c719b5f41
Ben <kbennani@rheinmetall.ca>
2020-02-09 21:53:00 -0500

    install fireBase

--- MedecinTakeManager/app/src/main/java/com/example/medecintakemanager/AddDataActivity.java
+++ MedecinTakeManager/app/src/main/java/com/example/medecintakemanager/AddDataActivity.java
@@ -3,6 +3,8 @@ package com.example.medecintakemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddDataActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AddDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_data);
+        Spinner spinnerNames = (Spinner) findViewById(R.id.spinnerNames);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(AddDataActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.Names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        spinnerNames.setAdapter(myAdapter);
     }
 }
--- MedecinTakeManager/app/src/main/java/com/example/medecintakemanager/HistoryActivity.java
+++ MedecinTakeManager/app/src/main/java/com/example/medecintakemanager/HistoryActivity.java
@@ -1,14 +1,75 @@
 package com.example.medecintakemanager;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    DatabaseReference mRef;
+    ListView nameArrayList;
+    ArrayList<String> myArrayList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        final ArrayAdapter<String> myAdapter = new ArrayAdapter<>(HistoryActivity.this, android.R.layout.simple_list_item_1,
+                myArrayList);
+
+
+        nameArrayList = findViewById(R.id.listView1);
+        nameArrayList.setAdapter(myAdapter);
+
+        myAdapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+
+        mRef = FirebaseDatabase.getInstance().getReference();
+
+        mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.getValue(String.class);
+                myArrayList.add(value);
+                myAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
